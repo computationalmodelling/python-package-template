ea76aba4126b08554257126f9c536d85cacbe611
Thomas Kluyver <thomas@kluyver.me.uk>
2016-05-13 14:13:27 +0100

    Basic package structure

---
+++ package_template/arith.py
@@ -0,0 +1,3 @@
+def add(x, y):
+    """Add two numbers"""
+    return x + y
---
+++ package_template/tests/test_arith.py
@@ -0,0 +1,5 @@
+from package_template import add
+
+def test_add():
+    assert add(2, 2) == 4
+    assert add(-2, 7) == 5
